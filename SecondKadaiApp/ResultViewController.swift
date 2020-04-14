35d12b9347b2c58cebdffc7369fe151d122db1a6
ruisasaki0723 <ruitohoku96@gmail.com>
2020-04-14 22:55:29 +0900

    課題「あいさつアプリ」を終わらせた。

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 佐々木瑠唯 on 2020/04/13.
+//  Copyright © 2020 Rui.Sasaki. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var text:String? = ""
+    //遷移元で入力された文字列を受け取る
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        label.text = "あなたの名前は\(text!)です"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.text = nameText.text
+        // 入力された文字列を遷移先の変数textに渡す
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+    }
+    
 }
 
